0e862463e2e4f56f63ea833cf3a4441e38d0d70e
bhavesh1414patil <137416664+bhavesh1414patil@users.noreply.github.com>
2024-08-02 19:06:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,132 @@
-const form = document.getElementById('inputs');
 
-var totalUser = [];
+if(!localStorage.getItem('currUser')){
+    location.href='../login/index.html';
+}
+const itemContainer = document.querySelector('.items');
+const listContainer = document.querySelector('.list-container');
+const totalPrice = document.getElementById('total-price');
+var cartItem=[];
 
-form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    // console.log('hello');
+if(localStorage.getItem('cartArr')){
+    let myArr =JSON.parse(localStorage.getItem('cartArr'));
 
-    const firstName = document.getElementById('first-name').value;
-    const lastName = document.getElementById('last-name').value;
-    const email = document.getElementById('email').value;
-    const pass = document.getElementById('pass').value;
-    const confPass = document.getElementById('confirm-pass').value;
+    cartItem=myArr;
 
-    if(!firstName || !lastName || !email || !pass || !confPass){
-        document.getElementById('message').style.display='inline';
-        document.getElementById('message').setAttribute('class','red')
-        document.getElementById('message').innerText='Error :All Fields are Mandatory.'
-        return;
+    showCartItem(cartItem);
+    
+}
+else{
+    totalPrice.innerHTML='0';
+}
+
+
+
+function showCartItem(Arr){
+    itemContainer.innerHTML='';
+    listContainer.innerHTML='';
+
+    if(cartItem.length==0){
+        itemContainer.innerHTML=`
+        <h3 style='text-align: center;;'>No products found in Cart</h3>
+        `;
+        console.log(totalPrice);
+        totalPrice.innerHTML='0';
     }
 
-    if(pass!=confPass){
-        document.getElementById('message').style.display='inline';
-        document.getElementById('message').setAttribute('class','red')
-        document.getElementById('message').innerText='Error :Passwrod and Confirm Password Should be same.'
-        return;
-    }
 
-    var user={
-        firstName:firstName,
-        lastName:lastName,
-        email:email,
-        password:pass
-    }
 
-    let flag=false;
-    if(localStorage.getItem('totalUser')){
-        // console.log("hello");
-        totalUser=JSON.parse(localStorage.getItem('totalUser'));
-        totalUser.forEach((user)=>{
-            if(user.email==email){
-                flag=true;
-                document.getElementById('message').style.display='inline';
-                document.getElementById('message').setAttribute('class','red')
-                document.getElementById('message').innerText='Error :User Already Exist.';
-            }
-        })
-    }
-    if(flag==true){
-        //user already exist;
-        return;
-    }
+    Arr.forEach((ele,index)=>{
+        itemContainer.innerHTML+=`
+        <div class="item">
+        <img src="${ele.image}" alt="Item" />
+        <div class="info">
+          <div style="margin-bottom: 10px; font-weight:600">${ele.title}</div>
+          <div style="font-weight:bold" class="row">
+            <div class="price">$${ele.price}</div>
+          </div>
+          <div style='margin-top:10px;' class="row">Rating: ${Math.floor(ele.rating.rate)}</div>
+        </div>
+        <button id="addBtn" onClick='removeFromCart(${ele.id})'>Remove From Cart</button>
+      </div>
+        `;
 
-    totalUser.push(user);
-    // console.log("user",user);
-    // console.log(totalUser);
+        listContainer.innerHTML+=`
+        <div style="display: flex; justify-content: space-between; margin-bottom: 20px; gap:20px">
+         <div><strong>${index+1}.<strong>  ${ele.title}</div>
+         <div>$${ele.price}</div>
+        </div>
+        `
 
-    localStorage.setItem('totalUser',JSON.stringify(totalUser));
 
-    document.getElementById('message').style.display='inline';
-    document.getElementById('message').setAttribute('class','green')
-    document.getElementById('message').innerText='User Successfully Added';
+    })
 
-    form.reset();
+    totalPrice.innerHTML=totalPriceFunc();
 
-    setTimeout(()=>{  
-       location.href='../login/index.html';
-    },1500);
 
-})
+}
+
+function removeFromCart(id){
+    console.log("remove");
+    let itemToRemove;
+    let indexToRemove;
+    cartItem.forEach((item,index)=>{
+        if(item.id==id){
+            itemToRemove=item;
+            indexToRemove=index;
+        }
+    })
+    console.log(itemToRemove);
+    cartItem.splice(indexToRemove,1);
+
+    localStorage.setItem('cartArr',JSON.stringify(cartItem));
+    showCartItem(cartItem);
+}
+
+if(cartItem.length==0){
+    itemContainer.innerHTML=`
+    <h3 style='text-align: center;;'>No products found in the Cart</h3>
+    `;
+}
+
+function totalPriceFunc(){
+    return cartItem.reduce((acc,item)=>{
+        return acc+item.price;
+    },0)
+}
+
+// Link for the documentation:
+// https://razorpay.com/docs/payments/payment-gateway/web-integration/standard/build-integration
+
+// Add button code documentation:
+// https://razorpay.com/docs/payments/payment-gateway/web-integration/standard/build-integration#code-to-add-pay-button
+
+document.getElementById("rzp-button1").onclick = function (e) {
+    var options = {
+      key: "rzp_test_PV1oQ0oMtgXOsq", // Enter the Key ID generated from the Dashboard
+      amount: totalPriceFunc() * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: "INR",
+      name: "MyShop Checkout",
+      description: "This is your order", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      theme: {
+        color: "#000",
+      },
+      image:
+        "https://www.mintformations.co.uk/blog/wp-content/uploads/2020/05/shutterstock_583717939.jpg",
+    };
+  
+    var rzpy1 = new Razorpay(options);
+    rzpy1.open();
+    // clear mycart - localStorage
+    localStorage.removeItem('cartArr');
+
+    cartItem=[];
+
+    showCartItem(cartItem);
+
+  };
+  
+
+
+
+
 
